fe81294273be45504f71b2d0a83c9fbd704a12fa
STIL-PC-006 <imtiazakil@gmail.com>
2020-01-13 17:45:20 +0600

    update branch test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Ajadi Jobs update
+                    Laravel Ajadi Jobs update branch test
                 </div>
 
                 <div class="links">
