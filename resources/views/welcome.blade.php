2a2ded06084206689133e1392c0e65f075cae27a
STIL-PC-006 <imtiazakil@gmail.com>
2020-01-13 17:39:51 +0600

    akil commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Ajadi Jobs
                 </div>
 
                 <div class="links">
